54eb381a44de8796d966329b4e6476a8a2916c55
GUNER Yusuf <yusuf_guner@msn.com>
2021-03-08 15:22:59 +0100

    Add pom.xml

---
+++ src/main/java/zero_gaspi/model/App.java
@@ -0,0 +1,13 @@
+package zero_gaspi.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
