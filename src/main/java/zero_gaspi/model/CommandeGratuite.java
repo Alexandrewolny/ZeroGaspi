14497c6e38e9e0d0e3de9f73ce6eee50e47c0d2f
GUNER Yusuf <yusuf_guner@msn.com>
2021-03-08 16:45:21 +0100

    Mapping de paiementet commandes

--- src/main/java/zero_gaspi/model/CommandeGratuite.java
+++ src/main/java/zero_gaspi/model/CommandeGratuite.java
@@ -1,5 +1,58 @@
 package zero_gaspi.model;
 
-public class CommandeGratuite {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "commande_gratuite")
+public class CommandeGratuite {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@OneToOne
+	@JoinColumn(name="id")
+	private Commande commande;
+	
+	public CommandeGratuite() {
+		super();
+	}
+	
+	public CommandeGratuite(Long id, Commande commande) {
+		super();
+		this.id = id;
+		this.commande = commande;
+	}
+
+	public CommandeGratuite(Long id, Commande commande) {
+		super();
+		this.id = id;
+		this.commande = commande;
+	}
+
+	public CommandeGratuite(Commande commande) {
+		super();
+		this.commande = commande;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Commande getCommande() {
+		return commande;
+	}
+
+	public void setCommande(Commande commande) {
+		this.commande = commande;
+	}
+	
+	
 }
--- src/main/java/zero_gaspi/model/Paiement.java
+++ src/main/java/zero_gaspi/model/Paiement.java
@@ -1,5 +1,45 @@
 package zero_gaspi.model;
 
-public class Paiement {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="paiement")
+public class Paiement {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Temporal(TemporalType.DATE)
+	private Date horodatage;
+	/*@OneToOne
+	@JoinColumn(name="id")*/
+	@Transient
+	private CommandePayante commandePayante;
+	
+	public Paiement() {
+		super();
+	}
+
+	public Paiement(Long id, Date horodatage, CommandePayante commandePayante) {
+		super();
+		this.id = id;
+		this.horodatage = horodatage;
+		this.commandePayante = commandePayante;
+	}
+
+	public Paiement(Date horodatage, CommandePayante commandePayante) {
+		super();
+		this.horodatage = horodatage;
+		this.commandePayante = commandePayante;
+	}
+	
+	
 }
